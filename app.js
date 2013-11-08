7da5141b541d0312af1cc16a53f1f6010f900b0c
Magnetic_dud <mail@dandandin.it>
2013-11-08 22:47:24 +0100

    creating a model

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ StackMob.init({
   publicKey: "648d7f86-44a0-408a-b0e3-b3b56925115f",
   apiVersion: 0
 });
+
+//i define my class
+//associated the model with the schema
+var Names = StackMob.Model.extend({
+	schemaName: 'names'  //they say the schemaName must be lowercase
+});
+
+//create new instance of Names
+var names = new Names({
+	name: 'John',
+	surname: 'Smith',
+	address: 'inexistent place 44322 mycity',
+	reputable: false
+});
+
+//send to stackmob
+names.create({
+	success: function(model,result,options) {console.debug(model.toJSON());},
+	error: function(model,error,options) {}
+});
